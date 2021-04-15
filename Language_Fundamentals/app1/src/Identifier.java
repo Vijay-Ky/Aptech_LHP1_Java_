8205d85543a3caa0a8778d59a68a409e3ef4861a
Vijay_Ky <vijayky007@gmail.com>
2021-04-15 15:52:41 +0530

    updated

---
+++ Language_Fundamentals/app1/src/Identifier.java
@@ -0,0 +1,53 @@
+class _ABC
+{
+}
+class $ABC
+{
+}
+class _$ABC_$
+{
+}
+//class 0Hello
+//{
+//}
+class Hello0
+{
+}
+class Hello0Hello
+{
+}
+
+//class !Hello
+//{
+//}
+/*
+class Hello!
+{
+}
+class @Hello
+{
+}
+*/
+
+class Abbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+{
+}
+class HelloAndWelcomeToJavaCourse
+{
+	public static void main(String[] args)
+	{
+		System.out.println("from HelloAndWelcomeToJavaCourse");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
